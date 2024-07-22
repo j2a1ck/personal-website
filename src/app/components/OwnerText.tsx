b649b5f4ced3f999c513c85361a93eb093b4db96
Jack <jack.70aw@gmail.com>
2024-07-22 15:03:23 +0330

    add social media icon

--- src/app/components/OwnerText.tsx
+++ src/app/components/OwnerText.tsx
@@ -1,19 +1,26 @@
-const OwnerText: React.FC = () => {
-    return (
-      //display text
-      <div> 
-        <h1 className="text-white"> My name is</h1>
-        <h2 className="text-navy-blue pt-4  px-8 lg:text-8xl md:text-8xl text-5xl font-bold">
-          Jack
-        </h2>
-        <h3 className="text-gray-lite pt-12 text-xs w-25 h-10 lg:text-2xl md:text-base">
-          newcomer to front-end development
-        </h3>
-        <h3 className="text-gray-lite pt-4 text-xs w-auto h-auto lg:text-2xl lg:pt-9 md:text-base md:pt-7">
-          Exploring of the front-end.
-        </h3>
-        </div>
-    );
-}
+import SocialMedia from "./SocialMedia";
+import { sanchez } from "../layout"; 
 
+// OwnerText component
+const OwnerText: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-white"> My name is</h1>
+      <h2
+        className={`text-navy-blue pt-4 px-8 lg:text-9xl md:text-9xl text-6xl font-extrabold ${sanchez.className}`} 
+      >
+        Jack
+      </h2>
+      <h3 className="text-gray-lite pt-12 text-xs w-25 h-10 lg:text-2xl md:text-base">
+        newcomer to front-end development
+      </h3>
+      <h3 className="text-gray-lite pt-4 text-xs w-auto h-auto lg:text-2xl lg:pt-9 md:text-base md:pt-7">
+        Exploring of the front-end.
+      </h3>
+      <div className="pt-3 lg:pt-6 md:pt-5 px-5">
+        <SocialMedia />
+      </div>
+    </div>
+  );
+};
 export default OwnerText;
---
+++ src/app/components/SocialMedia.tsx
@@ -0,0 +1,47 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faTelegram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+// Array of social media link objects
+const SocialMediaLinks = [
+  {
+    href: "https://t.me/j2a1ckbot",
+    icon: (
+      <FontAwesomeIcon icon={faTelegram} className="h-5 w-max lg:h-7 md:h-7" />
+    ),
+    label: "Telegram",
+  },
+  {
+    href: "https://github.com/j2a1ck",
+    icon: (
+      <FontAwesomeIcon icon={faGithub} className="h-5 w-max lg:h-7 md:h-7" />
+    ),
+    label: "Github",
+  },
+  {
+    href: "https://www.linkedin.com/in/j2a1ck/",
+    icon: (
+      <FontAwesomeIcon icon={faLinkedin} className="h-5 w-max lg:h-7 md:h-7" />
+    ),
+    label: "Linkedin",
+  },
+];
+
+// SocialMedia component
+const SocialMedia: React.FC = () => {
+  return (
+    // display social media icons and link to their sites
+    <div className="flex justify-items-start">
+      {SocialMediaLinks.map((link, index) => (
+        <a className="px-2" key={index} href={link.href} target="_blank">
+          {link.icon}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default SocialMedia;
