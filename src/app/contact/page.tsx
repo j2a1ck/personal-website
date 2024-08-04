5bd375358924389408536b970b1cafcd30c99158
Jack <jack.70aw@gmail.com>
2024-08-04 12:21:56 +0330

    fix: responsiveness in contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -35,7 +35,7 @@ function handleClick(): void {
 const Contact: React.FC = () => {
   return (
     <div className="flex flex-col items-center">
-      <div className="m-auto mt-4 flex h-20 w-20 justify-center overflow-hidden rounded-full md:h-40 md:w-40 lg:h-48 lg:w-48">
+      <div className="m-auto mt-6 flex h-44 w-44 justify-center overflow-hidden rounded-full md:h-40 md:w-40 lg:mt-4 lg:h-48 lg:w-48">
         <Image
           priority={true}
           src={ownerImage}
@@ -43,13 +43,13 @@ const Contact: React.FC = () => {
           className="object-cover"
         />
       </div>
-      <h1 className="flex justify-center pt-2 font-bold text-white-white lg:pt-3 lg:text-lg">
+      <h1 className="flex justify-center pt-7 font-bold text-white-white lg:pt-3 lg:text-lg">
         Jack
       </h1>
-      <h2 className="flex justify-center font-serif text-xs font-extralight text-white lg:text-sm">
+      <h2 className="flex justify-center font-serif text-sm font-extralight text-white lg:text-sm">
         give me your hands son
       </h2>
-      <div className="m-auto mt-5 flex w-36 justify-center justify-items-center space-x-2 lg:w-52">
+      <div className="m-auto mt-7 flex w-48 justify-center justify-items-center space-x-2 lg:mt-5 lg:w-52">
         {socialMediaLinks.map((link) => (
           <a
             href={link.url}
@@ -61,25 +61,25 @@ const Contact: React.FC = () => {
           </a>
         ))}
       </div>
-      <div className="mt-11 flex justify-center font-serif text-sm text-white lg:mt-9">
+      <div className="mt-20 flex justify-center font-serif text-sm text-white lg:mt-9">
         <a href="/cv.pdf" download="cv.pdf">
-          <button className="bg-black-dark h-6 w-36 rounded-3xl border hover:bg-black lg:h-9 lg:w-56">
+          <button className="h-7 w-44 rounded-3xl border bg-black-dark hover:bg-black lg:h-9 lg:w-56">
             Resume
           </button>
         </a>
       </div>
-      <div className="mt-2 flex justify-center font-serif text-sm text-white">
+      <div className="mt-4 flex justify-center font-serif text-sm text-white lg:mt-2">
         <button
           onClick={handleClick}
-          className="bg-black-dark h-6 w-36 rounded-3xl border hover:bg-black lg:h-9 lg:w-56"
+          className="h-7 w-44 rounded-3xl border bg-black-dark hover:bg-black lg:h-9 lg:w-56"
         >
           Email
         </button>
       </div>
-      <h3 className="mb-auto mt-8 font-sans text-white lg:mt-5"> other </h3>
-      <div className="mt-1 flex justify-center font-serif text-sm text-white">
+      <h3 className="mb-auto mt-24 font-sans text-white lg:mt-5"> other </h3>
+      <div className="mt-3 flex justify-center font-serif text-sm text-white">
         <a href="https://t.me/ja9ck">
-          <button className="h-7 w-36 rounded-3xl border bg-zinc-950 hover:bg-black lg:h-10 lg:w-56">
+          <button className="h-8 w-44 rounded-3xl border bg-zinc-950 hover:bg-black lg:h-10 lg:w-56">
             Telegram channel
           </button>
         </a>
