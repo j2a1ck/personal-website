b60ca9b66a3b31b5ccf35d0a8ddfda0dcbea5cc9
Jack <jack.70aw@gmail.com>
2024-08-05 17:48:49 +0330

    fix: deploy error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import type { Viewport } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
